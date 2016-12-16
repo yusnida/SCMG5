012ccdbffd539587f3923be2f8fff39268f2d8c6
wenjun727 <ccwin7270@gmail.com>
2016-12-16 10:51:38 +0800

    error testing
    $ is missing

--- myDB.php
+++ myDB.php
@@ -4,7 +4,7 @@ $username = "username";
 $password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
